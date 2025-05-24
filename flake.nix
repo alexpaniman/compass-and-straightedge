dfad736df717fd585cd2ed0b4a1de0824be8606f
alexpaniman <alexpaniman@gmail.com>
2025-05-24 22:13:25 +0300

    build: make flake.nix cross-platform

--- flake.nix
+++ flake.nix
@@ -4,55 +4,55 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixgl.url = "github:nix-community/nixGL";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixgl }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ nixgl.overlay ];
-        config.allowUnfree = true;
-      };
-    in
-  {
-
-    packages.${system}.default = pkgs.clangStdenv.mkDerivation {
-      name = "compass-and-straightedge";
-      src = ./.;
-
-      nativeBuildInputs = with pkgs; [
-        clang-tools
-
-        cmake
-        ninja
-
-        m4
-        makeWrapper
-      ];
-
-      buildInputs = with pkgs; [ glew glfw3 ];
-
-      cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
-
-      postFixup = ''
-        cp -r "$src/res" "$out"
-
-        executable_name="compass-and-straightedge"
-
-        wrapper="$out/bin/''${executable_name}"
-        wrapped="$out/bin/.''${executable_name}-wrapped"
-
-        mv "$wrapper" "$wrapped"
-
-        echo "#! @shell@ -e"                                         > "$wrapper"
-        echo "export GLNIMAN_RES_HOME='$out/res'"                   >> "$wrapper"
-        echo "${pkgs.nixgl.auto.nixGLDefault}/bin/nixGL '$wrapped'" >> "$wrapper"
-
-        chmod +x "$wrapper"
-        substituteAllInPlace "$wrapper"
-      '';
-    };
-
-  };
+  outputs = { self, nixpkgs, nixgl, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nixgl.overlay ];
+          config.allowUnfree = true;
+        };
+      in
+      {
+        packages.default = pkgs.clangStdenv.mkDerivation {
+          name = "compass-and-straightedge";
+          src = ./.;
+  
+          nativeBuildInputs = with pkgs; [
+            clang-tools
+  
+            cmake
+            ninja
+  
+            m4
+            makeWrapper
+          ];
+  
+          buildInputs = with pkgs; [ glew glfw3 ];
+  
+          cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
+  
+          postFixup = ''
+            cp -r "$src/res" "$out"
+  
+            executable_name="compass-and-straightedge"
+  
+            wrapper="$out/bin/''${executable_name}"
+            wrapped="$out/bin/.''${executable_name}-wrapped"
+  
+            mv "$wrapper" "$wrapped"
+  
+            echo "#! @shell@ -e"                                         > "$wrapper"
+            echo "export GLNIMAN_RES_HOME='$out/res'"                   >> "$wrapper"
+            echo "${pkgs.nixgl.auto.nixGLDefault}/bin/nixGL '$wrapped'" >> "$wrapper"
+  
+            chmod +x "$wrapper"
+            substituteAllInPlace "$wrapper"
+          '';
+        };
+      }
+    );
 }
